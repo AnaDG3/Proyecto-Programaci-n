26ebd4b9cf0e638ed2a518c1a2feeaa4f9ac946b
AnaDG3 <anadominguezgonzalez3@gmail.com>
2026-02-03 11:51:44 +0100

    correciones

--- src/main/java/aventura/domain/Nota.java
+++ src/main/java/aventura/domain/Nota.java
@@ -5,8 +5,8 @@ public class Nota extends Item implements Leible{
     private String contenido;
 
     //Constructor
-    public Nota(String nombre, String descripcion, String contenido) {
-        super(nombre,descripcion);
+    public Nota(String nombre, String descripcion, String contenido, boolean visible) {
+        super(nombre,descripcion, visible);
         this.contenido = contenido;
     }
 
